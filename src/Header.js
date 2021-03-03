79b4f937f750265ffb9f5e96f76eb21331dbf14d
Minh Ngo <ngo.minh0224@gmail.com>
2021-03-02 16:24:10 -0800

    react

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div>
+        <NavLink exact activeClassName="active" to="/">
+          Home
+        </NavLink>
+        <NavLink exact activeClassName="active" to="/create">
+          Create
+        </NavLink>
+        <NavLink exact activeClassName="active" to="/kitkats">
+          List
+        </NavLink>
+      </div>
+    );
+  }
+}
---
+++ src/Spinner.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Spinner extends Component {
+  render() {
+    return (
+      <div>
+        <img
+          src="https://thumbs.gfycat.com/ArtisticShoddyKudu-size_restricted.gif"
+          alt="spinner"
+        />
+      </div>
+    );
+  }
+}
